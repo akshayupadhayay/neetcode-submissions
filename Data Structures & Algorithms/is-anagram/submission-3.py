91e39ede2361c53e2f61e825afff76f4df7ff58a
Akshay Upadhayay <akshay.upadhayay@gmail.com>
2026-09-17 12:23:01 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if Counter(s) == Counter(t):
+            return True
+        return False
+
+        # if len(s) != len(t):
+        #     return False
+
+        # d1 = {}
+
+        # for char in s:
+        #     d1[char] = d1.get(char, 0) + 1
+        # for char in t:
+        #     d1[char] = d1.get(char, 0) - 1
+
+        # for k, v in d1.items():
+        #     if v != 0:
+        #         return False
+        
+        # return True
+
+        
